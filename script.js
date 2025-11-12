0e3224a9e284f3bb1a49072d6cf39277c7d4a5fd
Matioli_Senai <guilhermemsbr@gmail.com>
2025-11-12 09:19:40 -0300

    UPDATE: SCRIPT.JS botao excluir

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let botaoMaiusculo = document.getElementById("botaoMaiusculo")
 let botaoMinusculo = document.getElementById("botaoMinusculo")
 let botaoCapitalizar = document.getElementById("botaoCapitalizar")
+let botaoExcluir = document.getElementById("botaoExcluir")
     
 
 botaoMaiusculo.addEventListener("click", function () {
@@ -19,4 +20,10 @@ botaoCapitalizar.addEventListener("click", function () {
     let inputTexto = document.querySelector("#inputTexto").value
 
     document.querySelector("#resultado").innerHTML = `Resultado: ${inputTexto.replace(/\b\w+/g, palavra => palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase())}`
+})
+
+botaoExcluir.addEventListener("click", function () {
+    document.querySelector("#inputTexto").value = ""
+    
+    document.querySelector("#resultado").innerHTML = "Resultado:"
 })
